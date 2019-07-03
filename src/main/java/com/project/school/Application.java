9668c2f94466f749c293373bb4ae1c3d6504dfb0
konrad caban <konrad_caban@o2.pl>
2019-07-03 22:49:11 +0200

    Projekt wraz z modelami oraz przykladowym dodanie do bazy danych nauczyciela

---
+++ src/main/java/com/project/school/Application.java
@@ -0,0 +1,17 @@
+package com.project.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.project.school.*;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(Application.class, args);
+		System.out.println("LOL");
+	}
+
+}
